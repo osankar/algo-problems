4c5f4df8f875879eb906121bdb1bed5f76da4d3b
Sankar Ojili <62626825+osankar@users.noreply.github.com>
2021-05-17 20:49:01 -0400

    Update BestSum.java

--- src/main/java/sankar/ojili/java/algos/dynamic-programming/src/san/ojili/bestsum/BestSum.java
+++ src/main/java/sankar/ojili/java/algos/dynamic-programming/src/san/ojili/bestsum/BestSum.java
@@ -29,6 +29,9 @@ public class BestSum {
             return null;
         }
         List<Integer> bestCombination = null;
+        //divide the problem into subproblems' In this case sum is reduced to sum-current_number.
+        //Noe the problem reduced to find the combination to get to teh sum of sum-current_number
+        //For every recurise call the sum goes down till it becomes 0.
         for (int num : nums) {
             int reminderSum = sum-num;
             List<Integer> combination = bestSum(reminderSum, nums);
@@ -41,7 +44,10 @@ public class BestSum {
         }
         return bestCombination;
     }
-
+    
+    /**
+    * Same as brute-force recursive but avoids repeated solving of subproblems by memoizing the results of the subproblems that are already solved.
+    */
     static List<Integer> bestSumMemo(int sum, int[] nums, Map<Integer, List<Integer>> memo) {
        // System.out.println(memo);
         if (memo.containsKey(sum)) {
@@ -65,21 +71,27 @@ public class BestSum {
                 }
             }
         }
+        //memoize the result of the subproblem
         memo.put(sum, bestCombination);
         return bestCombination;
     }
 
-    static List<Integer> bestSumTabular(int targetSum, int[] nums) {
+    static List<Integer> bestSumTabular(int targetSum, int[] nums) { //let sum be m abd nums be of size n, the time complexity is O(mXn) and space complexity is also o(mxN)
         List<List<Integer>> table = new ArrayList<>();
+        //initialize all elements of the list with null; make all positions unreachable;
         for (int i=0; i<=targetSum; i++) {
             table.add(null);
         }
+        //position 0 is reserved for base case i.e. way to reach sum of zero. 
         table.set(0, new ArrayList<>());
-        for(int i=0; i< targetSum; i++) {
+        for(int i=0; i< targetSum; i++) { 
+            //A non reachable position indicates an impossible sum.
+            //As we move from 0th postion to Nth position, we populate the position with the possible combination of number that sums up to the position/index.
+            //We start 0th with empty List by the time we move to a Kth position, we would have various possibilities to reach all the K-1 positions. 
             if (table.get(i) == null) {
                 continue;
-            }
-            for (int num : nums) {
+            } 
+            for (int num : nums) { 
                 if (i + num > targetSum) {
                    continue;
                 }
